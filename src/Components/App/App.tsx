a1473079618aec16774537892331070c1f227339
Adam Meza <Adammeza95@gmail.com>
2023-06-06 12:52:18 -0400

    vreate header

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,85 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { Main } from '../Main/Main';
-import { cleanData } from '../../utilites';
-import { Header } from '../Header/Header';
-import { ErrorComp } from '../Error/Error';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import { fetchAllCards } from '../../apiCalls';
+import { Header } from '../Header/Header';
+import { Gameboard } from '../Gameboard/Gameboard';
+import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
+import { Scoreboard } from '../Scoreboard/Scoreboard';
+import { Definition } from '../Definition/Definition';
 
-export type PokeCard = {
-  image: string;
-  name: string;
-  types: string[];
-  cardId: string;
-};
+function App() {
+  const [error, setError] = useState(null)
+  const [center, setCenter] = useState('')
+  const [letters, setLetters] = useState('')
+  const [wordlist, setWords] = useState<String[]>([])
 
-const App: React.FC = () => {
-  const [allCards, setCards] = useState<PokeCard[]>([]);
-  const [favorites, setFavorites] = useState<PokeCard[]>([]);
-  const [error, setError] = useState<Error | null>(null);
-  const [loading, setLoading] = useState(true);
+  const checkGuess = (guess : String) => {
+    return wordlist.find(word => guess === word) ? true : false ;
+  }
 
-  const chooseCard = (pokemon: PokeCard) => {
-    const newFavState = [...favorites, pokemon];
-    setFavorites(newFavState);
-  };
-
-  const removeFavorite = (cardToRemove: PokeCard) => {
-    const updatedFavs = favorites.filter(
-      (card) => card.cardId !== cardToRemove.cardId
-    );
-    setFavorites(updatedFavs);
-  };
-
-  useEffect(() => {
+  const fetchData = async () => {
     try {
-      fetchAllCards().then((data) => {
-        const cardData = cleanData(data.data);
-        setCards(cardData);
-        setLoading(false);
-      });
-    } catch (err: any) {
-      setError(err);
-    }
-  }, []);
+      const response = await fetch('https://freebee.fun/cgi-bin/today')
+      if (response.ok) {
+        const json = await response.json()
+        const {letters, wordlist, center} = json
 
+        setCenter(center)
+        setLetters(letters)
+        setWords(wordlist)
+      } else {
+        throw new Error()
+      }
+
+    } catch(error : any) {
+      setError(error)
+    }
+  }
+  
+  useEffect(() => {
+    fetchData()
+  }, [])
+ 
   return (
-    <section className="main">
-      <Header />
-      {loading && <div className="loading-text">Loading...</div>}
-      {error && (
-        <div className="error-text">
-          {error.name}: {error.message}
-        </div>
-      )}
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => (
-            <Main
-              data={allCards}
-              chooseCard={chooseCard}
-              removeFavorite={removeFavorite}
-            />
-          )}
-        />
-        <Route
-          exact
-          path="/favorites"
-          render={() => (
-            <Main
-              data={favorites}
-              chooseCard={chooseCard}
-              removeFavorite={removeFavorite}
-            />
-          )}
-        />
-        <Route path="*" render={() => <ErrorComp />} />
-      </Switch>
-    </section>
+    <div className="App">
+      <Header/>
+      <main>
+        {error && <ErrorMessage message={error}/>}
+
+        { !error && <Gameboard 
+        checkGuess={checkGuess}
+        letters ={letters} 
+        center ={center} 
+        words ={wordlist} />}
+      </main>
+      <aside className='aside'>
+        <Scoreboard />
+        <Definition />
+      </aside>
+    </div>
+    
   );
-};
+}
 
 export default App;
