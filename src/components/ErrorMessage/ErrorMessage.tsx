82095db44510a24a6c38550575b323ba59114b0b
Adam Meza <Adammeza95@gmail.com>
2023-06-09 14:43:44 -0400

    test

---
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './error-message.css'
+interface ErrorProps {
+  message: String
+}
+
+export const ErrorMessage : React.FC<ErrorProps> = ({message}) => {
+  return (
+    <div className="error-card">
+      <p>{message}</p>
+    </div>
+  )
+}
